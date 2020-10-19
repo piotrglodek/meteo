5485c1b8c4f2e9c40162fb67767bbea9a378e6b9
Piotr Glodek <pioglodek@gmail.com>
2020-10-19 23:20:15 +0200

    Create Hamburger

---
+++ src/components/Header/Hamburger.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+// icon
+import { ReactComponent as MenuSvg } from '../../static/icons/menu.svg';
+
+export const Hamburger = ({ toggleMenu }) => {
+  return (
+    <StyledButton onClick={toggleMenu}>
+      <StyledMenuIcon />
+    </StyledButton>
+  );
+};
+
+const StyledButton = styled.button`
+  background: none;
+  margin: 0;
+  padding: 0;
+  border: none;
+  outline: none;
+  display: grid;
+  place-items: center;
+  width: 2.8rem;
+  height: 2.8rem;
+`;
+
+const StyledMenuIcon = styled(MenuSvg)`
+  fill: ${({ theme: { color } }) => color.primary};
+`;
+
+Hamburger.propTypes = {
+  toggleMenu: PropTypes.func.isRequired,
+};
