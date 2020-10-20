e9b0e2649498f045615fc036a65c2e039bc93d00
Piotr Glodek <pioglodek@gmail.com>
2020-10-20 21:32:48 +0200

    Move and change Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,25 +1,24 @@
 import React from 'react';
 import styled from 'styled-components';
+// Hook
+import { useMenu } from '../hooks/useMenu';
 // components
 import { Hamburger } from './Hamburger';
-import { Navigation } from './Navigation';
-import { Location } from './Location';
-// hook
-import { useMenu } from '../../hooks/useMenu';
+import { Nav } from './Nav';
+import { SearchBar } from './SearchBar';
 
 export const Header = () => {
   const [isOpen, openMenu, closeMenu] = useMenu();
   return (
     <StyledHeader>
-      <Hamburger toggleMenu={openMenu} />
-      <Navigation toggleMenu={closeMenu} isOpen={isOpen} />
-      <Location />
+      <Hamburger openMenu={openMenu} />
+      <Nav isOpen={isOpen} closeMenu={closeMenu} />
+      <SearchBar />
     </StyledHeader>
   );
 };
 
 const StyledHeader = styled.header`
-  width: 100%;
   padding: 2rem 1.8rem 1rem;
   display: flex;
   justify-content: space-between;
