184f55d8bf2c6f31535ba38ea71251f1bfd79b0b
Piotr Glodek <pioglodek@gmail.com>
2020-10-21 14:01:18 +0200

    Add fetching data new layout structure

--- src/view/Weather.js
+++ src/view/Weather.js
@@ -1,14 +1,31 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import styled from 'styled-components';
 // Components
 import { WeatherWidget, Header } from '../components';
+// Api
+import { useQuery } from 'react-query';
+import { fetchWeather } from '../api/fetchWeather';
+// Store
+import { StoreContext } from '../store';
+// Loading skelet
+import { WidgetSkelet, NoCity } from '../components';
 
 export const Weather = () => {
+  const [state] = useContext(StoreContext);
+  const { isLoading, data } = useQuery(state.city, fetchWeather);
   return (
     <StyledView>
       <StyledContainer>
         <Header />
-        <WeatherWidget />
+        <StyledMain>
+          {isLoading ? (
+            <WidgetSkelet />
+          ) : data.cod === '200' ? (
+            <WeatherWidget weather={data} />
+          ) : (
+            <NoCity />
+          )}
+        </StyledMain>
       </StyledContainer>
     </StyledView>
   );
@@ -24,4 +41,11 @@ const StyledContainer = styled.div`
   width: 100%;
   max-width: 90rem;
   margin: 0 auto;
+`;
+
+const StyledMain = styled.main`
+  width: 100%;
+  display: flex;
+  flex-flow: row wrap;
+  margin-top: 2rem;
 `;
